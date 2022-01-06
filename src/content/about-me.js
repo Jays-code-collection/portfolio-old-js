351e0658ce2decd248282325924faf9a85a6d72b
Jay Sharma <jay.indra.sharma@gmail.com>
2022-01-06 19:13:19 +0000

    changes

--- src/content/about-me.js
+++ src/content/about-me.js
@@ -14,6 +14,7 @@ export default [
       // '"<a rel="noopener" href="/jaysharma.pdf" download target="_blank">jaysharma.pdf</a>"',
         '"<a rel="noopener" href="https://jays-code-collection.github.io/portfolio/public/jaysharma.pdf" target="_blank">jaysharma.pdf</a>"',
         
+        
     },
     {
       input: 'Jay.education',
