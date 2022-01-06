d090f2b9b099961a61a13aa140b2f5f5ad59ad6d
Jay Sharma <jay.indra.sharma@gmail.com>
2022-01-06 18:40:18 +0000

    Didn't save

--- src/content/about-me.js
+++ src/content/about-me.js
@@ -12,7 +12,7 @@ export default [
       input: 'Jay.cv',
       return:
       // '"<a rel="noopener" href="/jaysharma.pdf" download target="_blank">jaysharma.pdf</a>"',
-        '"<a rel="noopener" href="https://jays-code-collection.github.io/portfolio/public/jaysharma.pdf" target="_blank">jaysharma.pdf</a>"',
+        '"<a rel="noopener" href="https://jays-code-collection.github.io/portfolio/jaysharma.pdf" target="_blank">jaysharma.pdf</a>"',
     },
     {
       input: 'Jay.education',
